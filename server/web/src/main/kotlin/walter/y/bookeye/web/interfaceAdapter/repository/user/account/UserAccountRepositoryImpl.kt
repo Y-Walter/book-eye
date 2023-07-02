aa7cf7d2fb31073b230bda0b0243f541c1b1bbef
DESKTOP-DBUM9AD\yukun <yukunimoto@keb.biglobe.ne.jp>
2023-07-02 17:59:22 +0900

    UserAccountRepository を追加

---
+++ server/web/src/main/kotlin/walter/y/bookeye/web/interfaceAdapter/repository/user/account/UserAccountRepositoryImpl.kt
@@ -0,0 +1,32 @@
+﻿package walter.y.bookeye.web.interfaceAdapter.repository.user.account
+
+import org.springframework.security.crypto.password.PasswordEncoder
+import org.springframework.stereotype.Repository
+import walter.y.bookeye.web.domain.user.account.model.PrincipalName
+import walter.y.bookeye.web.domain.user.account.model.UserAccountEntity
+import walter.y.bookeye.web.domain.user.account.model.UserAccountId
+import walter.y.bookeye.web.domain.user.account.model.UserAccountPassword
+import walter.y.bookeye.web.domain.user.account.repository.UserAccountRepository
+import walter.y.bookeye.web.domain.user.model.UserId
+
+@Repository
+class UserAccountRepositoryImpl(
+    private val passwordEncoder: PasswordEncoder
+) : UserAccountRepository {
+    override fun getOrNull(principalName: PrincipalName): UserAccountEntity? {
+        val userAccount = try {
+            UserAccountEntity(
+                userId = UserId(1L),
+                userAccountId = UserAccountId(1L),
+                principalName = principalName,
+                userAccountPassword = UserAccountPassword.Source("Account-Password-Used-4-Development-Only").let {
+                    UserAccountPassword.Builder(it, passwordEncoder).build()
+                },
+                isEnabled = true
+            )
+        } catch (ex: Exception) {
+            null
+        }
+        return userAccount
+    }
+}
--- server/web/src/main/kotlin/walter/y/bookeye/web/useCase/account/GetAccountUseCaseImpl.kt
+++ server/web/src/main/kotlin/walter/y/bookeye/web/useCase/account/GetAccountUseCaseImpl.kt
@@ -1,32 +1,16 @@
 ﻿package walter.y.bookeye.web.useCase.account
 
-import org.springframework.security.crypto.password.PasswordEncoder
-import walter.y.bookeye.web.domain.user.account.model.UserAccountEntity
-import walter.y.bookeye.web.domain.user.account.model.UserAccountId
-import walter.y.bookeye.web.domain.user.account.model.UserAccountPassword
-import walter.y.bookeye.web.domain.user.model.UserId
+import walter.y.bookeye.web.domain.user.account.repository.UserAccountRepository
 import walter.y.bookeye.web.useCase.account.input.GetAccountInput
 import walter.y.bookeye.web.useCase.account.output.GetAccountOutput
 import walter.y.bookeye.web.useCase.annotation.UseCase
 
 @UseCase
 class GetAccountUseCaseImpl(
-    private val passwordEncoder: PasswordEncoder
+    private val userAccountRepository: UserAccountRepository
 ) : GetAccountUseCase {
     override fun execute(input: GetAccountInput): GetAccountOutput {
-        val userAccount = try {
-            UserAccountEntity(
-                userId = UserId(1L),
-                userAccountId = UserAccountId(1L),
-                principalName = input.principalName,
-                userAccountPassword = UserAccountPassword.Source("Account-Password-Used-4-Development-Only").let {
-                    UserAccountPassword.Builder(it, passwordEncoder).build()
-                },
-                isEnabled = true
-            )
-        } catch (ex: Exception) {
-            null
-        }
+        val userAccount = userAccountRepository.getOrNull(principalName = input.principalName)
         return GetAccountOutput(account = userAccount)
     }
 }
